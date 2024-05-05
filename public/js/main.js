84391820c6feee0d2b34c5e55b2f80d1684cd271
Celeste-Rhoades <celesteisthebest82@gmail.com>
2024-05-05 14:51:35 -0600

    updated comments

--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,7 @@ const itemCompleted = document.querySelectorAll(".item span.completed");
 Array.from(deleteBtn).forEach(element => {
   element.addEventListener("click", deleteItem);
 });
-// add event listener to evry todo-item
+// add event listener to evry todo-item Array.from in the case is not needed
 Array.from(item).forEach(element => {
   element.addEventListener("click", markComplete);
 });
@@ -14,26 +14,37 @@ Array.from(item).forEach(element => {
 Array.from(itemCompleted).forEach(element => {
   element.addEventListener("click", markUnComplete);
 });
-// delets 1 item clicked on from database.
+// deletes 1 item thats been clicked on from the database
 async function deleteItem() {
   // grabbing the todo item
+  //   this referres to the item that is being listened to (button). Parent node navigates through the DOM and finds the parent element of this.
+  // childnode[1] grabs the second child node return a nodelist which contain text and spaces , comments
+  // this practice pulls the second element because of the line break
+  // a better option would be this.parentNode.children[0].innertext
   const itemText = this.parentNode.childNodes[1].innerText;
   try {
+    // will fetch the API on our server.js , its firing a request to delete item URL, server is going to listen and is then going to delete that item becuase on he method (IF server specifies),
     const response = await fetch("deleteItem", {
+      // method defines what part of crud we are accessing
       method: "delete",
+      //   headers is the type of data we are sending in the body
       headers: { "Content-Type": "application/json" },
+      //   on the server side this is where we are getting the req.body from as well as itemFromJs
       body: JSON.stringify({
         itemFromJS: itemText,
       }),
     });
+    // defines data and awaits response from JSON then reloads the URL
     const data = await response.json();
     console.log(data);
     location.reload();
+    // if there is an error in the try block and error will console
   } catch (err) {
     console.log(err);
   }
 }
-
+// going to reference the server markComplete
+// going to fetch to the API ( server.js) look for the markComplete with the method put, written in JSON, to get the response body.ItemFromJs
 async function markComplete() {
   const itemText = this.parentNode.childNodes[1].innerText;
   try {
@@ -44,6 +55,7 @@ async function markComplete() {
         itemFromJS: itemText,
       }),
     });
+    // This is going to wait for JSOn to finish and logs it or returns and error
     const data = await response.json();
     console.log(data);
     location.reload();
@@ -51,7 +63,7 @@ async function markComplete() {
     console.log(err);
   }
 }
-
+// This is going to fetch the API and look for the PUT markUncomplete. its going to go to the parentNode, SKip the whitespace, and access childNode[1](because its skipping whitespace) Its going to look at the put response, written in JSON ,and get the res.body.itemFromJs
 async function markUnComplete() {
   const itemText = this.parentNode.childNodes[1].innerText;
   try {
@@ -62,6 +74,7 @@ async function markUnComplete() {
         itemFromJS: itemText,
       }),
     });
+    // waits for the body to resolve then reloads or rejects
     const data = await response.json();
     console.log(data);
     location.reload();
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // pulling all our dependcies and declairing a port
-// Thisis used to import express from module to application. Require is part of node.js that allows you to import modules. In this case express modules
+// This is used to import express from module to application. Require is part of node.js that allows you to import modules. In this case express modules
 const express = require("express");
 // defining the app variable to the function call to Express module
 const app = express();
@@ -17,16 +17,17 @@ let db,
   dbName = "todo";
 //connects MongoDB database and takes 2 arguments. dbConnectionStr - is the connetion string(defined earlier has MongoDB servers address, database name and possibly auth details) { useUnifiedTopology: true } is becoming outdated replace with {useNewUrlParser: true} it doesnt directly replace it but recommended for connection setup
 // .then this uses a promise to handle async connection process once connection happens the callback(.then())is exucuted. CLient parameter represents connection client which interacts with the database
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }).then(
-  client => {
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+  .then(client => {
     //this line logs indicating that the connection has been established successfully
     console.log(`Connected to ${dbName} Database`);
-    //is a method used to get a reference to the databse. it can then be used preform operations on the database such as querying collections or inserting documens
+    //is a method used to get a reference to the databse. it can then be used preform operations on the database such as querying collections or inserting documents
     db = client.db(dbName);
-  }
-);
+  })
+  .catch(error => console.error(error));
+
 // set up middleware template engine
-// sets view engine (combining fixed HTML with dynamic data.  displaying dynamic data on web pages by allowing you to embed variables, conditions, loops, and other logic directly within your HTML templates. )
+// sets view engine (combining fixed HTML with dynamic data.  displaying dynamic data on web pages by allowing you to embed variables, conditions, loops, and other logic directly within your HTML templates. ) ejs
 // Express will look for an EJS file with the corresponding name in the views directory and render it as HTML.
 app.set("view engine", "ejs");
 // Express to serve static files from a directory named public
@@ -35,50 +36,67 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 // another middleware(handles everything between request and response) Handles the JSONdata sent to the body of POST requests (found in RESTFul API and AJAX)
 app.use(express.json());
+
 // defines route handler for Express.js for HTTP GET requests made to the root URL(/) of app
-// app.get -allows you to define how your app responds to different typs of hhtp requests (crud) to specific URLs
+// app.get -allows you to define how your app responds to different typs of http requests (crud) to specific URLs
 // where root handler is defined (located at the end of URl)
-// async function  serves as a callbackfor route handler takes 2 params
+// async function  serves as a callback for route handler takes 2 params
 // request - HTTP request. It contains information about the request, such as headers, query parameters, and the body of the request.
 // response - formulate the HTTP response that will be sent back to the client. You can use methods on this object to send data back to the client,
 app.get("/", async (request, response) => {
   // this retrieves all docs from "to-dos" collection and stores them in array
-  // db.collection("todos")- this acceses collections within the database (mongodb, nonde.js)
+  // db.collection("todos")- this acceses collections within the database (mongodb, node.js)
   // .find() - create a query that will retrieve documents from collections(without any arguments, find() retrieves all documents in the collection.)
   // .toArray() - converts the results of the query(which is a cursor- meaning enables traveresal over records in database) into an array
-  const todoItems = await db.collection("todos").find().toArray();
-  // db.collection - accesses the 'to-do' collection in database
-  const itemsLeft = await db
-    .collection("todos")
-    //called on 'to-dos' collectioin to count the docs that matcha specified filter. The filter { completed: false } specifies that only documents where the completed field is false should be counted.
-    .countDocuments({ completed: false });
-  // renders a view and sends the rendered html string to client.
-  // index.ejs - is the template file you are rendering
-  // object that contains local variable for the view.
-  //
-  response.render("index.ejs", { items: todoItems, left: itemsLeft });
-  // db.collection('todos').find().toArray()
-  // .then(data => {
-  //     db.collection('todos').countDocuments({completed: false})
-  //     .then(itemsLeft => {
-  //         response.render('index.ejs', { items: data, left: itemsLeft })
-  //     })
-  // })
-  // .catch(error => console.error(error))
+  try {
+    const todoItems = await db.collection("todos").find().toArray();
+    // db.collection - accesses the 'to-do' collection in database
+    const itemsLeft = await db
+      .collection("todos")
+      //called on 'to-dos' collection to count the docs that matcha specified filter. The filter { completed: false } specifies that only documents where the completed field is false should be counted.
+      .countDocuments({ completed: false });
+
+    // renders a view and sends the rendered html string to client.
+    // index.ejs - is the template file you are rendering
+    // object that contains local variable for the view.
+    //{ items: todoItems, left: itemsLeft }: This is an object that contains the local variables for the view.
+    // items - contains array of items and assigned value of todoItems
+    // left- contains number of todo items that are not completed obtained from completed:false collection
+    response.render("index.ejs", { items: todoItems, left: itemsLeft });
+  } catch (err) {
+    console.error(err);
+    response.status(500).send({ error: "Something failed!" });
+    // db.collection('todos').find().toArray()
+    // .then(data => {
+    //     db.collection('todos').countDocuments({completed: false})
+    //     .then(itemsLeft => {
+    //         response.render('index.ejs', { items: data, left: itemsLeft })
+    //     })
+    // })
+    // .catch(error => console.error(error
+  }
 });
+
 // makes a post api request to addOne entry
+// app.post - defines the route handler for HTT post requests. the callback function(request, response)and response is executed
+// db.collection- named in the case todo is accessed
 app.post("/addTodo", (request, response) => {
   db.collection("todos")
-    //go to collection and insert one object with key of thing and completed
+    //  insertOne - this inserts a new document to the collection
+    //  thing- is coming from the http request and came from the body
+    // completed: false - tells whether it has been completed or not false means the todo item is not completed
     .insertOne({ thing: request.body.todoItem, completed: false })
+    // .then-a promise/callback that logs "todo Added"
     .then(result => {
       console.log("Todo Added");
-      //refresh users screen after post submission
+      //redirect the user to / (home)
       response.redirect("/");
-    })
-    .catch(error => console.error(error));
+    });
+  //  if the promise is rejected  because an error occured during operation returns an error
 });
-//makes an update request to the server to mark smething as complete
+
+//this starts the route handler put to update aspefic document in mongo db
+//marking the item on the todo list complete
 app.put("/markComplete", (request, response) => {
   //connect to data base
   db.collection("todos")
@@ -86,20 +104,24 @@ app.put("/markComplete", (request, response) => {
       //set item specified in request body to complete with the specified options
       { thing: request.body.itemFromJS },
       {
+        // onbjct form for mongo db syntax. Marking the item as completed
         $set: {
           completed: true,
         },
       },
       {
+        //sorting the id from last to first (determine if needed sort)
         sort: { _id: -1 },
+        // if the document is does not exist dont create one because of it being false
         upsert: false,
       }
     )
     .then(result => {
-      //send back results to show request was
+      //going to mark as todoitem as complete. log and respond to the client side server
       console.log("Marked Complete");
       response.json("Marked Complete");
     })
+    // if result doesnt happen correctly we throw an error
     .catch(error => console.error(error));
 });
 //makes an update to the server to mark something incomplete
